8e90d6b2da4fcf0992d1bc397022e9bb1c4d648f
josebtan <josebtan@users.noreply.github.com>
2026-09-07 21:54:56 +0000

    Fix: sacar resto de import CameraX en MainActivity

--- app/src/main/java/com/astrofoto/app/MainActivity.kt
+++ app/src/main/java/com/astrofoto/app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.astrofoto.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.camera.camera2.interop.ExperimentalCamera2Interop
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -11,7 +10,6 @@ import com.astrofoto.app.capture.CaptureScreen
 import com.astrofoto.app.ui.theme.AstrofotoTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalCamera2Interop::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
